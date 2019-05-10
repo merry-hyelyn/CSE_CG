2733b28afe2d6b6d441157b66e82d0dcc80938b0
merry-Hyelyn <h1335@naver.com>
2019-05-11 00:02:23 +0900

    Fix car.c

--- Homework/Car.c
+++ Homework/Car.c
@@ -1,6 +1,8 @@
 #include <glut.h>
-float yangle = 0;
-//int yangle = 0;
+
+int yangle = 0;
+GLfloat xangle=0.0;
+GLfloat wheel_x=0.0;
 
 void init()
 {
@@ -20,32 +22,91 @@ void reshape(int new_w, int new_h)
 	glMatrixMode(GL_MODELVIEW);
 	glutPostRedisplay();
 }
+
 void draw_car()
 {
+	//*****차 아랫부분에서 앞부분*****//
 	glBegin(GL_QUADS);
-	glVertex3f( 0.6, 0.5,0.6);
-	glVertex3f(0.6, 0.2,0.6);
-	glVertex3f(1.8, 0.2, 0.6);
-	glVertex3f(1.8,0.5,0.6);
+	glVertex3f(-2.0, 0.0, 0.0);
+	glVertex3f(2.0, 0.0, 0.0);
+	glVertex3f(2.0, 0.8, 0.0);
+	glVertex3f(-2.0, 0.8, 0.0);
 
-	/* bottom of cube*/
-	glVertex3f( 0.6,0.2,0.6);
-	glVertex3f(0.6,0.2,0.2);
-	glVertex3f(1.8,0.2,0.2);
-	glVertex3f( 1.8,0.2,0.6);
+	//*****오른쪽 부분*****//
+	glVertex3f(2.0, 0.0, 0.0);
+	glVertex3f(2.0, 0.8, 0.0);
+	glVertex3f(2.0, 0.8, -0.8);
+	glVertex3f(2.0, 0.0, -0.8);
+	
+	//*****왼쪽 부분*****//
+	glVertex3f(-2.0, 0.0, 0.0);
+	glVertex3f(-2.0, 0.8, 0.0);
+	glVertex3f(-2.0, 0.8, -0.8);
+	glVertex3f(-2.0, 0.0, -0.8);
 
-	/* back of cube.*/
-	glVertex3f(0.6,0.5,0.2);
-	glVertex3f(0.6,0.2,0.2);
-	glVertex3f(1.8,0.2,0.2);
-	glVertex3f(1.8,0.5,0.2);
+	//*****뒷부분*****//
+	glVertex3f(-2.0, 0.0, -0.8);
+	glVertex3f(2.0, 0.0, -0.8);
+	glVertex3f(2.0, 0.8, -0.8);
+	glVertex3f(-2.0, 0.8, -0.8);
 	glEnd();
+
+	//*****차 윗부분에서 앞부분******//
+	glBegin(GL_QUADS);
+	glVertex3f(-1.0, 0.8, 0.0);
+	glVertex3f(1.15, 0.8, 0.0);
+	glVertex3f(1.15, 1.3, 0.0);
+	glVertex3f(-1.0, 1.3, 0.0);
+
+	//왼쪽
+	glVertex3f(-1.0, 0.8, 0.0);
+	glVertex3f(-1.0, 1.3, 0.0);
+	glVertex3f(-1.0, 1.3, -0.4);
+	glVertex3f(-1.0, 0.8, -0.4);
+
+	//오른쪽
+	glVertex3f(1.15, 0.8, 0.0);
+	glVertex3f(1.15, 1.3, 0.0);
+	glVertex3f(1.15, 1.3, -0.4);
+	glVertex3f(1.15, 0.8, -0.4);
+
+	//뒤에
+	glVertex3f(1.15, 0.8, -0.4);
+	glVertex3f(1.15, 1.3, -0.4);
+	glVertex3f(-1.0, 1.3, -0.4);
+	glVertex3f(-1.0, 0.8, -0.4);
+	glEnd();
+
+	//****라이트*****//
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 1.0, 0.0);
+	glVertex3f(-2.0, 0.2, 0.0);
+	glVertex3f(-2.0, 0.5, 0.0);
+	glVertex3f(-2.15, 0.5, 0.0);
+	glVertex3f(-2.15, 0.2, 0.0);
+	glEnd();
+
+	//바퀴
+	glPushMatrix();
+	glTranslatef(-1.0, 0.0, 0.0);
+	glRotatef(wheel_x, 1.0, 0.0, 0.0);
+	glColor3f(0.0, 0.0, 0.0);
+	glutSolidTorus(0.07,0.2,10,25);
+
+	glTranslatef(1.95, 0.0, 0.0);
+	glRotatef(wheel_x, 1.0, 0.0, 0.0);
+	glutSolidTorus(0.07,0.2,10,25);
+	glPopMatrix();
 }
+
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glEnable(GL_DEPTH_TEST);
 
-	glColor3f(0.0, 0.0, 0.0);
+	glColor3f(0.0, 0.0, 1.0);
+	glTranslatef(xangle, 0.0, 0.0);
 	glRotatef(yangle, 0.0, 1.0, 0.0);
 	draw_car();
 
@@ -60,24 +121,46 @@ void popupMenu(int value)
 		glutPostRedisplay();
 	}
 }
-void keyboardProcess(unsigned char key, int x, int y)
+
+static void SpecialKeyFunc(int key, int x, int y)
+{
+	switch(key)
+	{
+	case GLUT_KEY_UP:
+		xangle += 0.05;
+		glutPostRedisplay();
+		break;
+
+	case GLUT_KEY_DOWN:
+		xangle -= 0.05;
+		glutPostRedisplay();
+		break;
+
+	case GLUT_KEY_RIGHT:
+	case GLUT_KEY_LEFT:
+	}
+}
+
+/*void keyboardProcess(unsigned char key, int x, int y)
 {
 	switch(key)
 	{
 	case 'y': 
-		yangle = (yangle + 5.0) % 360;
+		yangle=(yangle + 1) % 360;
+		glutPostRedisplay();
 		break;
 	case 'Y':
-		yangle = (yangle - 5.0 ) % 360;
+		yangle=(yangle - 1 ) % 360;
+		glutPostRedisplay();
 		break;
 	}
-	glutPostRedisplay();
-}
+	
+}*/
 
 int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
-   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
    glutInitWindowSize(900, 500);
    glutInitWindowPosition(100, 100);
    glutCreateWindow("자동차 그리기");
@@ -90,6 +173,7 @@ int main(int argc, char** argv)
    glutDisplayFunc(display);
    glutReshapeFunc(reshape);
    glutKeyboardFunc(keyboardProcess);
+   glutSpecialFunc(SpecialKeyFunc);
    //glutMouseFunc(mouseProcess);
    
    glutMainLoop();
